d95b4b5f0b2e876862b37d2e783285893d269928
eliasyeme <eliasyeme@gmail.com>
2023-11-21 13:34:05 +0300

    Added make it responsible

--- src/lib/components/Day1/Container.svelte
+++ src/lib/components/Day1/Container.svelte
@@ -1,5 +1,5 @@
 <div
-	class="flex min-w-[750px] flex-col gap-4 rounded-2xl border-8 border-white bg-[#F1E2F5] p-8 text-blue-950 shadow-2xl"
+	class="flex flex-col gap-4 rounded-2xl border-8 border-white bg-[#F1E2F5] p-8 text-blue-950 shadow-2xl md:min-w-[750px]"
 >
 	<slot />
 </div>
--- src/routes/day1/+layout.svelte
+++ src/routes/day1/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import Circle from '$lib/components/icons/Circle.svelte';
 	import { page } from '$app/stores';
+	import Close from '$lib/components/icons/Close.svelte';
+	import Hamberger from '$lib/components/icons/Hamberger.svelte';
+	let isNavShowing = false;
 </script>
 
 <div
@@ -11,22 +14,35 @@
 	</div>
 </div>
 
-<nav class="absolute left-0 top-0 flex w-full items-center justify-between p-6">
-	<a href="/day1">
+<nav
+	class="absolute left-0 top-0 flex w-full items-start justify-between p-6 md:items-center"
+	class:bg-white={isNavShowing}
+>
+	<a href="/day1" class:hidden={isNavShowing}>
 		<Circle content={'day1'} />
 	</a>
 
-	<ul class="flex flex-col gap-2 text-blue-950 md:flex-row">
-		<li>
-			<a href="/day1/tags" class:active={$page.url.pathname === '/day1/tags'}>Tags</a>
-		</li>
-		<li>
-			<a href="/day1/search" class:active={$page.url.pathname === '/day1/search'}>Search</a>
-		</li>
-		<li>
-			<a href="/day1/recent" class:active={$page.url.pathname === '/day1/recent'}>Recent</a>
-		</li>
-	</ul>
+	<div class="md:block" class:hidden={!isNavShowing}>
+		<ul class="flex flex-col gap-2 text-blue-950 md:flex-row">
+			<li>
+				<a href="/day1/tags" class:active={$page.url.pathname === '/day1/tags'}>Tags</a>
+			</li>
+			<li>
+				<a href="/day1/search" class:active={$page.url.pathname === '/day1/search'}>Search</a>
+			</li>
+			<li>
+				<a href="/day1/recent" class:active={$page.url.pathname === '/day1/recent'}>Recent</a>
+			</li>
+		</ul>
+	</div>
+
+	<button on:click={() => (isNavShowing = !isNavShowing)} class="text-blue-950 md:hidden">
+		{#if isNavShowing}
+			<Close />
+		{:else}
+			<Hamberger />
+		{/if}
+	</button>
 </nav>
 
 <style lang="postcss">
